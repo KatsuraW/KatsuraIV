2b49e39b9c75e5dd98287ad71067b672f1b521c0
emlag <edl926@g.harvard.edu>
2018-07-29 11:29:23 +0800

    Fix txt file name.

--- src/edu/cis/pset1/Main.java
+++ src/edu/cis/pset1/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         DNAStrand myDna = new DNAStrand();
         Mrna myMRNA = new Mrna();
-        myDna.readDNA("dnaSequenceLab1");
+        myDna.readDNA("dnaSequencePset1");
 
         myMRNA.createCopy(myDna.getDnaSequence());
         ArrayList<String> protein = Ribosome.createProtein(myMRNA.getMessengerDna());
